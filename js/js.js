c56aec79a03e01319d34eb08460fb0c4a680ae18
Artem Arshaniy <artem.arshaniy108@gmail.com>
2017-03-10 20:21:20 +0200

    add html css and js view part

--- js/js.js
+++ js/js.js
@@ -1,1 +1,30 @@
-console.log("efcdsx");
+var view = {
+
+  showCount: function (count) {
+    var elCount = document.getElementById('area_game__user_count--total');
+    elCount.innerHTML = count;
+  },
+  showMsg: function (msg) {
+    var elMessage = document.getElementById('rea_game__user-message--msg');
+    elMessage.innerHTML = msg;
+  },
+  showShip: function (id, color) {
+    var elShip = document.getElementById(id);
+    if (color == "red") {
+      elShip.setAttribute("class", "ship-red");
+    } else if (color == "blue") {
+      elShip.setAttribute("calss", "ship-blue");
+    }
+  },
+  showAsteroid: function (id) {
+    var elAsteroid = document.getElementById(id);
+    elAsteroid.setAttribute("class", "asteroid");
+  },
+  soundShot: function () {
+    var audio = document.getElementsByTagName('audio')[0];
+
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+  }
+}
